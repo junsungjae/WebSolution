0300f4e6eb4dc7f3e3a34a9e225e5aeb9d71deae
sungjae jun <jsjf2000@naver.com>
2021-10-27 02:00:57 +0900

    node js 시작

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('안녕 node js');
+  res.send('node js를 시작하자!');
 });
 
 app.listen(3000, function () {
